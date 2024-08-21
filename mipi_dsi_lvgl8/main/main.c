b38b7b361b2ffa5db0b07f4521d8f6bbdeb3bce7
ShallowGreen123 <2608653986@qq.com>
2024-08-21 11:50:01 +0800

    fix：Delete redundant files

---
+++ mipi_dsi_lvgl8/main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_mipi_dsi.h"
+#include "esp_ldo_regulator.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "lvgl.h"
+#include "lv_demos.h"
+#include "esp_amoled_rm69a10.h"
+
+
+
+void app_main(void)
+{
+    
+}
