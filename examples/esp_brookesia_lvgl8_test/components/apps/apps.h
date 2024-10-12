8822983fcd3e9a98a7d65c19b29cc44f52a129c1
ShallowGreen123 <2608653986@qq.com>
2024-10-12 16:37:09 +0800

    fix compile error

--- examples/esp_brookesia_lvgl8_test/components/apps/apps.h
+++ examples/esp_brookesia_lvgl8_test/components/apps/apps.h
@@ -2,6 +2,6 @@
 
 // #include "smart_gadget/SmartGadget.hpp"
 #include "music_player/MusicPlayer.hpp"
-#include "setting/Setting.hpp"
+// #include "setting/Setting.hpp"
 #include "game_2048/Game_2048.hpp"
 #include "calculator/Calculator.hpp"
